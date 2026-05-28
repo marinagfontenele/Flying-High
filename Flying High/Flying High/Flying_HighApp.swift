b982bbe665b1e8889d6973f783afe3625993ef81
Marina Gomes Fontenele <marinagomesfontenele2007@gmail.com>
2026-05-28 09:25:04 -0300

    first commit

---
+++ Flying High/Flying High/Flying_HighApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flying_HighApp.swift
+//  Flying High
+//
+//  Created by Marina Gomes Fontenele on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Flying_HighApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
