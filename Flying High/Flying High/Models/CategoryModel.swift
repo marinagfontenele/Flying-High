c731bcaca0a699b5bc6e96b3c04c573bd8eaef95
ulissesbon <ugoncalvesb@gmail.com>
2026-06-12 08:55:20 -0300

    feat: CategoryModel updated to Enum type

--- Flying High/Flying High/Models/CategoryModel.swift
+++ Flying High/Flying High/Models/CategoryModel.swift
@@ -6,16 +6,44 @@
 //
 
 import Foundation
-import SwiftData
+import SwiftUI
 
-@Model
-class CategoryModel {
+enum CategoryModel: String, CaseIterable, Codable{
+    case none = "Não definido"
+    case cleaning = "Limpeza"
+    case repair = "Reparo"
+    case other = "Outros"
+    case organization = "Organização"
     
-    @Attribute(.unique) var id: UUID
-    var title: String
-    
-    init(id: UUID, title: String) {
-        self.id = id
-        self.title = title
+    var displayName: String {
+        switch self {
+            case .none: return "Não definido"
+            case .cleaning: return "Limpeza"
+            case .repair: return "Reparo"
+            case .other: return "Outros"
+            case .organization: return "Organização"
+        }
     }
+    
+    var iconColor: Color {
+        switch self{
+            case .none: return .gray
+            case .cleaning: return .blue
+            case .repair: return .red
+            case .other: return .gray
+            case .organization: return .orange
+        }
+    }
+    
+    var iconSymbol: String {
+        switch self{
+        case .none: return "xmark.circle"
+        case .cleaning: return "trash"
+        case .repair: return "hammer"
+        case .other: return "questionmark.circle"
+        case .organization: return "folder.circle"
+        }
+    }
+    
 }
+
