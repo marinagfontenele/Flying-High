88e8aaba89384c65a8d22d4d018c61b34c96e0f6
Marina Gomes Fontenele <marinagomesfontenele2007@gmail.com>
2026-06-15 09:11:28 -0300

    fix: TimerView

--- Flying High/Flying High/Views/TimerView.swift
+++ Flying High/Flying High/Views/TimerView.swift
@@ -8,10 +8,15 @@
 import SwiftUI
 
 struct TimerView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
     @State private var isPaused: Bool = false
     @State private var isPresented: Bool = false
     @State var nextTask: String = "Comprar Pões"
-    @Environment(\.dismiss) var dismiss
+    @State private var isEnabled: Bool = false
+    @State private var isEnabled2: Bool = false
+    @State private var navigation: Bool = false
     
     var body: some View {
         NavigationStack{
@@ -43,16 +48,134 @@ struct TimerView: View {
                 Spacer(minLength: 0)
                 
                 Button {
+                    isEnabled.toggle()
+                } label: {
+                    Label("Finalizar Bloco", systemImage: "checkmark")
+                        .frame(maxWidth: .infinity)
+                        .padding(16)
+                        .font(.title2)
+                        .foregroundStyle(.white)
+                        .fontWeight(.bold)
+                }
+                .buttonStyle(.glassProminent)
+                .tint(.main)
+                .padding(.horizontal, 16)
+                
+//                NavigationLink(destination: EmptyView()) {
+//                    Label("Finalizar Bloco", systemImage: "checkmark")
+//                        .frame(maxWidth: .infinity)
+//                        .padding(16)
+//                        .font(.title2)
+//                        .foregroundStyle(.white)
+//                        .fontWeight(.bold)
+//                }
+//                .buttonStyle(.glassProminent)
+//                .tint(.main)
+//                .padding(.horizontal, 16)
+//                .simultaneousGesture(TapGesture().onEnded {
+//                    isEnabled.toggle()
+//                })
+                .alert("Tem certeza de que deseja finalizar essa tarefa?", isPresented: $isEnabled, actions: {
+                    VStack {
+                        Button("Cancelar", role: .cancel) {
+                            
+                        }
+                        .tint(.black)
+                        
+//                        Button("Finalizar e voltar para a tela inicial 2") {
+//                            navigation.toggle()
+//                        }
+//                        .buttonStyle(.glassProminent)
+//                        .tint(.main)
+//                        
+//                        if navigation {
+//                            ScheduleView()
+//                        }
+                        
+                       // NavigationLink(destination: ScheduleView()) {
+                            Button("Finalizar e voltar para a tela inicial") {
+                                dismiss()
+                                dismiss()
+                            }
+                            .buttonStyle(.glassProminent)
+                            .tint(.main)
+                      //  }
+                        
+                        NavigationLink(destination: EmptyView()) {
+                            Button("Finalizar e passar para a próxima tarefa") {
+                                
+                            }
+                            .buttonStyle(.glassProminent)
+                            .tint(.main)
+                        }
+                    }
+                }, message: {
+                    Text("Após finalizada, a tarefa não poderá mais ser retomada.")
+                })
+                
+//                Button {
+//                    isPresented.toggle()
+//                } label: {
+//                    Label("Informações", systemImage: "info.circle.fill")
+//                        .font(.body)
+//                        .fontWeight(.semibold)
+//                        .foregroundStyle(.white)
+//                        .padding(10)
+//                        .background(.main)
+//                        .clipShape(RoundedRectangle(cornerRadius: 1000))
+//                }
+//                .sheet(isPresented: $isPresented){
+//                    NavigationStack {
+//                        SheetTimerView()
+//                    }
+//                    .presentationDetents([.medium])
+//                    .presentationBackground(Color(.systemBackground))
+//                    .presentationBackgroundInteraction(.enabled)
+//                }
+            }
+            .animation(.default, value: isPresented)
+            .background(Color.background
+                .ignoresSafeArea())
+        }
+        .toolbar{
+            ToolbarItem(placement: .navigationBarLeading){
+                Button{
+                    isEnabled2.toggle()
+                } label: {
+                    Image(systemName: "chevron.backward")
+                        .fontWeight(.semibold)
+                        .foregroundColor(.black)
+                }
+                .tint(.white)
+                .alert("Tem certeza de que deseja finalizar essa tarefa?", isPresented: $isEnabled2, actions: {
+                    HStack {
+                        Button("Cancelar", role: .cancel) {
+                        //    dismiss()
+                        }
+                        .tint(.black)
+                        .foregroundStyle(Color(.systemGray2))
+                        
+                        Button("Finalizar") {
+                            dismiss()
+                            dismiss()
+                        }
+                        .buttonStyle(.glassProminent)
+                        .tint(.main)
+                    }
+                }, message: {
+                    Text("Após finalizada, a tarefa não poderá mais ser retomada.")
+                })
+            }
+                
+            ToolbarItem(placement: .navigationBarTrailing){
+                Button{
                     isPresented.toggle()
                 } label: {
-                    Label("Informações", systemImage: "info.circle.fill")
-                        .font(.body)
+                    Image(systemName: "info")
                         .fontWeight(.semibold)
-                        .foregroundStyle(.white)
-                        .padding(10)
-                        .background(.main)
-                        .clipShape(RoundedRectangle(cornerRadius: 1000))
                 }
+                .buttonStyle(.glassProminent)
+                .tint(.main)
                 .sheet(isPresented: $isPresented){
                     NavigationStack {
                         SheetTimerView()
@@ -62,22 +185,8 @@ struct TimerView: View {
                     .presentationBackgroundInteraction(.enabled)
                 }
             }
-            .animation(.default, value: isPresented)
-            .background(Color.background
-                .ignoresSafeArea())
         }
-        .toolbar{
-            ToolbarItem(placement: .navigationBarTrailing){
-                Button{
-                    
-                } label: {
-                    Image(systemName: "checkmark")
-                        .fontWeight(.semibold)
-                }
-                .buttonStyle(.glassProminent)
-                .tint(.main)
-            }
-        }
+        .navigationBarBackButtonHidden(true)
     }
 }
 
