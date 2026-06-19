566a3c1eab98caf8b21f9b7512a085e59b03598a
Davicbm <davicarvalho.b1@gmail.com>
2026-06-19 20:54:47 -0300

    feat: separate ArticleSheetView from TipsView

--- Flying High/Flying High/Views/TipsView.swift
+++ Flying High/Flying High/Views/TipsView.swift
@@ -52,94 +52,11 @@ struct TipsView: View {
             }
             .background(Color.background
                 .ignoresSafeArea())
-            .fullScreenCover(item: $selectedCard){ item in TelaCheiaPostView(content: item, tituloDoPost: "Artigo")
+            .fullScreenCover(item: $selectedCard){ item in ArticleSheetView(content: item)
             }
         }
     }
     
-}
-
-struct TelaCheiaPostView: View{
-    let content: ArticleModel
-    @Environment(\.dismiss) var dismiss
-    
-    let tituloDoPost: String
-    
-    var body:some View{
-        NavigationStack{
-            ScrollView{
-                VStack(alignment: .leading, spacing: 0){
-                    if (content.imageName != nil){
-                        ZStack {
-                            Image(content.imageName!)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width:180, height: 180)
-                            
-                        }
-                        .frame(height:280)
-                        .frame(maxWidth:.infinity)
-                    }
-                    
-                    VStack(alignment:.leading, spacing: 12){
-                        Text("\(content.title)")
-                            .font(.title)
-                            .bold()
-                            .foregroundColor(.primary)
-                        
-                        Text("\(content.subtitle)")
-                            .font(.title3)
-                            .foregroundColor(.primary)
-                            .padding(.bottom, 12)
-                        
-                        Text (hightlighTitles(content.text))
-                            .font(.body)
-                            .fontWeight(.regular)
-                            .foregroundColor(.primary)
-                            .lineSpacing(6)
-                    }
-                    .padding(24)
-                }
-                .ignoresSafeArea(edges: .top)
-                .toolbar{
-                    ToolbarItem(placement: .topBarTrailing){
-                        Button(action: {
-                            dismiss()
-                        }){
-                            Text("OK")
-                                .font(.headline)
-                                .foregroundColor(.main)
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-    func hightlighTitles(_ string: String) -> AttributedString {
-        var titles = string.components(separatedBy: .newlines).filter { line in
-            line.starts(with: "#")
-        }
-        titles = titles.map { title in
-            String(title.dropFirst())
-        }
-        
-        let string = string.replacingOccurrences(of: "#", with: "")
-        
-        var attributedString = try! AttributedString(
-            markdown: string,
-            options: .init(interpretedSyntax: .inlineOnlyPreservingWhitespace)
-        )
-        
-        for title in titles {
-            if let range = attributedString.range(of: title) {
-                attributedString[range].font = .title3
-                attributedString[range].inlinePresentationIntent = .stronglyEmphasized
-            }
-        }
-        
-        return attributedString
-    }
 }
 
 #Preview {
