08477495a79469f5b0809c0f02347cbafe88ea12
Davicbm <davicarvalho.b1@gmail.com>
2026-06-22 08:26:53 -0300

    feat: ScheduleCardView accessibility labels

--- Flying High/Flying High/Resources/Components/ScheduleCardView.swift
+++ Flying High/Flying High/Resources/Components/ScheduleCardView.swift
@@ -20,13 +20,13 @@ struct ScheduleCardView: View {
                         .font(.title3)
                         .fontWeight(.semibold)
                         .foregroundStyle(.text)
-                        .accessibilityLabel(Text("Título do cronograma"))
+                        .accessibilityLabel(Text("Título do cronograma: \(schedule.title)"))
                     
                     Label(schedule.totalTime.formatToAbbreviated(), systemImage: "timer")
                         .font(.body)
                         .fontWeight(.semibold)
                         .foregroundStyle(.text)
-                        .accessibilityLabel(Text("Duração do cronograma"))
+                        .accessibilityLabel(Text("Duração do cronograma: \(schedule.totalTime.formatToAbbreviated())"))
                     
                     CategoryTagView(category: schedule.category!)
                 }
@@ -39,6 +39,11 @@ struct ScheduleCardView: View {
             .background(.violet)
             .clipShape(RoundedRectangle(cornerRadius: 10))
         }
+        .accessibilityLabel(Text("Cronograma"))
+        .accessibilityAction {
+            <#code#>
+        }
+        
     }
 }
 
